3211fa671e5b026d9d1561ed05734ddb53e485e5
rlaalswhd <rlaalswhd200@naver.com>
2023-09-20 14:12:56 +0900

    Initial Commit

---
+++ SearchApp/SearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAppApp.swift
+//  SearchApp
+//
+//  Created by 김민종 on 2023/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
