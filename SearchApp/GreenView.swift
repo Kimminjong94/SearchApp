6d92bb96edf64d7d503ece5deee65d3a825edbd3
rlaalswhd <rlaalswhd200@naver.com>
2023-10-16 18:58:53 +0900

    commit

--- SearchApp/GreenView.swift
+++ SearchApp/GreenView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by 김민종 on 2023/10/06.
 //
-
+import Foundation
 import SwiftUI
 
 struct GreenView: View {
@@ -12,20 +12,14 @@ struct GreenView: View {
     @State var lastAmount: CGFloat = 1
     var body: some View {
         VStack {
-            Image("hoonImage3")
-                .resizable()
-                .scaledToFill()
-                .scaleEffect(currentAmount + lastAmount)
-                .gesture(
-                    MagnificationGesture()
-                        .onChanged { value in
-                            currentAmount = value - 1
-                        }
-                        .onEnded { value in
-                            lastAmount += currentAmount
-                            currentAmount = 0
-                        }
-                )
+            
+            ZoomableScrollView {
+                Image("hoonImage3")
+                    .resizable()
+                    .scaledToFill()
+            }
+         
+                
         }
     }
 }
@@ -33,5 +27,55 @@ struct GreenView: View {
 struct GreenView_Previews: PreviewProvider {
     static var previews: some View {
         GreenView()
+    }
+}
+
+struct ZoomableScrollView<Content: View>: UIViewRepresentable {
+    private var content: Content
+
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+
+    func makeUIView(context: Context) -> UIScrollView {
+        // set up the UIScrollView
+        let scrollView = UIScrollView()
+        scrollView.delegate = context.coordinator  // for viewForZooming(in:)
+        scrollView.maximumZoomScale = 20
+        scrollView.minimumZoomScale = 1
+        scrollView.bouncesZoom = true
+
+        // create a UIHostingController to hold our SwiftUI content
+        let hostedView = context.coordinator.hostingController.view!
+        hostedView.translatesAutoresizingMaskIntoConstraints = true
+        hostedView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        hostedView.frame = scrollView.bounds
+        scrollView.addSubview(hostedView)
+
+        return scrollView
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(hostingController: UIHostingController(rootView: self.content))
+    }
+
+    func updateUIView(_ uiView: UIScrollView, context: Context) {
+        // update the hosting controller's SwiftUI content
+        context.coordinator.hostingController.rootView = self.content
+        assert(context.coordinator.hostingController.view.superview == uiView)
+    }
+
+    // MARK: - Coordinator
+
+    final class Coordinator: NSObject, UIScrollViewDelegate {
+        var hostingController: UIHostingController<Content>
+
+        init(hostingController: UIHostingController<Content>) {
+            self.hostingController = hostingController
+        }
+
+        func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+            return hostingController.view
+        }
     }
 }
